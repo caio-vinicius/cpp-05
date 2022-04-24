2878cb90f636b85929b90dc05f76434c81678ef2
caio-vinicius <contato@caiovinicius.dev.br>
2022-04-24 15:41:48 -0300

    ex02: remove pre/pos increment/decrement removed, g++ -> c++, adjust tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -112,74 +112,38 @@ int form(void) {
 
 int bureaucrat(void) {
     {
-        std::cout << "--- 1 test" << std::endl;
-        Bureaucrat bureaucrat("caio", 1);
-        std::cout << bureaucrat << std::endl;
-        bureaucrat++;
-        std::cout << bureaucrat << std::endl;
-        ++bureaucrat;
+        std::cout << "--- 1 TEST" << std::endl;
+        Bureaucrat bureaucrat("caio", 151);
         std::cout << bureaucrat << std::endl;
     }
     {
         std::cout << "--- 2 TEST" << std::endl;
-        Bureaucrat bureaucrat("caio", 150);
-        std::cout << bureaucrat << std::endl;
-        bureaucrat--;
-        std::cout << bureaucrat << std::endl;
-        --bureaucrat;
+        Bureaucrat bureaucrat("caio", 0);
         std::cout << bureaucrat << std::endl;
     }
     {
         std::cout << "--- 3 TEST" << std::endl;
         Bureaucrat bureaucrat("caio", 15);
         std::cout << bureaucrat << std::endl;
-        bureaucrat++;
-        std::cout << bureaucrat << std::endl;
-        ++bureaucrat;
+        bureaucrat.incrementGrade();
         std::cout << bureaucrat << std::endl;
     }
     {
         std::cout << "--- 4 TEST" << std::endl;
         Bureaucrat bureaucrat("caio", 15);
         std::cout << bureaucrat << std::endl;
-        bureaucrat--;
-        std::cout << bureaucrat << std::endl;
-        --bureaucrat;
-        std::cout << bureaucrat << std::endl;
-    }
-    {
-        std::cout << "--- 5 TEST" << std::endl;
-        Bureaucrat bureaucrat("caio", 151);
-        std::cout << bureaucrat << std::endl;
-    }
-    {
-        std::cout << "--- 6 TEST" << std::endl;
-        Bureaucrat bureaucrat("caio", 0);
-        std::cout << bureaucrat << std::endl;
-    }
-    {
-        std::cout << "--- 7 TEST" << std::endl;
-        Bureaucrat bureaucrat("caio", 15);
-        std::cout << bureaucrat << std::endl;
-        bureaucrat.incrementGrade();
-        std::cout << bureaucrat << std::endl;
-    }
-    {
-        std::cout << "--- 8 TEST" << std::endl;
-        Bureaucrat bureaucrat("caio", 15);
-        std::cout << bureaucrat << std::endl;
         bureaucrat.decrementGrade();
         std::cout << bureaucrat << std::endl;
     }
     {
-        std::cout << "--- 9 TEST" << std::endl;
+        std::cout << "--- 5 TEST" << std::endl;
         Bureaucrat bureaucrat("caio", 1);
         std::cout << bureaucrat << std::endl;
         bureaucrat.incrementGrade();
         std::cout << bureaucrat << std::endl;
     }
     {
-        std::cout << "--- 10 TEST" << std::endl;
+        std::cout << "--- 6 TEST" << std::endl;
         Bureaucrat bureaucrat("caio", 150);
         std::cout << bureaucrat << std::endl;
         bureaucrat.decrementGrade();
